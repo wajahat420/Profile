1a253721f90996f71a145f144ba607dd26253919
wajahat420 <shayanmustafa11@gmail.com>
2020-10-29 22:25:27 +0000

    setting env

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require("express")
 const app = express()
 const bodyParser = require("body-parser")
 const mongoose = require('mongoose')
+const nodemailer = require('nodemailer');
+const dotenv = require("dotenv");
+dotenv.config();
+
 const Msg = require("./model")
 
 app.use(express.static(__dirname + '/public'));
@@ -43,6 +47,31 @@ app.use("/post",(req,res)=>{
         console.log("else",msg)
     }
 })
+
+// email 
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASS // naturally, replace both with your real credentials or an application-specific password
+    }
+  });
+  
+  const mailOptions = {
+    from: process.env.EMAIL,
+    to: 'sarwat4007473@cloud.neduet.edu.pk',
+    subject: 'Subject Of Mail',
+    text: 'Does this works???'
+  };
+  
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+
 const port = process.env.PORT || 5002
 // form-control-submit-button
 app.listen(port, () => console.log(`server running on port ${port}`))
